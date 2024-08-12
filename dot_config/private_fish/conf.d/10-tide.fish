8ee81fdd50db5d3fc5b09caad2f15a139d05096d
Josh Reed <jriddy@gmail.com>
2024-08-12 13:23:17 -0400

    fish: dont configure tide if its not installed

--- dot_config/private_fish/conf.d/10-tide.fish
+++ dot_config/private_fish/conf.d/10-tide.fish
@@ -1,2 +1,3 @@
 #!/usr/bin/fish
-tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Many icons' --transient=No
+type -q tide
+and tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Many icons' --transient=No
